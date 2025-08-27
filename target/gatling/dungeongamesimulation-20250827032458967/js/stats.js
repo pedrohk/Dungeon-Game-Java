78f354f7cb3ad7cad0971329bb49a2fe245fcf68
Pedro <pedrohk@gmail.com>
2025-08-27 00:29:37 -0300

    new report

---
+++ target/gatling/dungeongamesimulation-20250827032458967/js/stats.js
@@ -0,0 +1,785 @@
+var stats = {
+    type: "GROUP",
+name: "All Requests",
+path: "",
+pathFormatted: "group_missing-name-b06d1",
+stats: {
+    "name": "All Requests",
+    "numberOfRequests": {
+        "total": "750",
+        "ok": "0",
+        "ko": "750"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "179",
+        "ok": "-",
+        "ko": "179"
+    },
+    "meanResponseTime": {
+        "total": "8",
+        "ok": "-",
+        "ko": "8"
+    },
+    "standardDeviation": {
+        "total": "28",
+        "ok": "-",
+        "ko": "28"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "51",
+        "ok": "-",
+        "ko": "51"
+    },
+    "percentiles4": {
+        "total": "168",
+        "ok": "-",
+        "ko": "168"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 750,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "6.198",
+        "ok": "-",
+        "ko": "6.198"
+    }
+},
+contents: {
+"req_solve-complex-d-4517b": {
+        type: "REQUEST",
+        name: "Solve Complex Dungeon Game Static Request",
+path: "Solve Complex Dungeon Game Static Request",
+pathFormatted: "req_solve-complex-d-4517b",
+stats: {
+    "name": "Solve Complex Dungeon Game Static Request",
+    "numberOfRequests": {
+        "total": "50",
+        "ok": "0",
+        "ko": "50"
+    },
+    "minResponseTime": {
+        "total": "35",
+        "ok": "-",
+        "ko": "35"
+    },
+    "maxResponseTime": {
+        "total": "179",
+        "ok": "-",
+        "ko": "179"
+    },
+    "meanResponseTime": {
+        "total": "93",
+        "ok": "-",
+        "ko": "93"
+    },
+    "standardDeviation": {
+        "total": "58",
+        "ok": "-",
+        "ko": "58"
+    },
+    "percentiles1": {
+        "total": "51",
+        "ok": "-",
+        "ko": "51"
+    },
+    "percentiles2": {
+        "total": "167",
+        "ok": "-",
+        "ko": "167"
+    },
+    "percentiles3": {
+        "total": "179",
+        "ok": "-",
+        "ko": "179"
+    },
+    "percentiles4": {
+        "total": "179",
+        "ok": "-",
+        "ko": "179"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 50,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.413",
+        "ok": "-",
+        "ko": "0.413"
+    }
+}
+    },"req_solve-dungeon-g-944a6": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[-2",
+path: "Solve Dungeon Game Dynamic Request - [[-2",
+pathFormatted: "req_solve-dungeon-g-944a6",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[-2",
+    "numberOfRequests": {
+        "total": "88",
+        "ok": "0",
+        "ko": "88"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "125",
+        "ok": "-",
+        "ko": "125"
+    },
+    "meanResponseTime": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "standardDeviation": {
+        "total": "13",
+        "ok": "-",
+        "ko": "13"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "23",
+        "ok": "-",
+        "ko": "23"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 88,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.727",
+        "ok": "-",
+        "ko": "0.727"
+    }
+}
+    },"req_solve-dungeon-g-c2582": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[10]]",
+path: "Solve Dungeon Game Dynamic Request - [[10]]",
+pathFormatted: "req_solve-dungeon-g-c2582",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[10]]",
+    "numberOfRequests": {
+        "total": "88",
+        "ok": "0",
+        "ko": "88"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "124",
+        "ok": "-",
+        "ko": "124"
+    },
+    "meanResponseTime": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "standardDeviation": {
+        "total": "13",
+        "ok": "-",
+        "ko": "13"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "20",
+        "ok": "-",
+        "ko": "20"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 88,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.727",
+        "ok": "-",
+        "ko": "0.727"
+    }
+}
+    },"req_solve-dungeon-g-4e159": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[-10]]",
+path: "Solve Dungeon Game Dynamic Request - [[-10]]",
+pathFormatted: "req_solve-dungeon-g-4e159",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[-10]]",
+    "numberOfRequests": {
+        "total": "88",
+        "ok": "0",
+        "ko": "88"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "meanResponseTime": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "standardDeviation": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 88,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.727",
+        "ok": "-",
+        "ko": "0.727"
+    }
+}
+    },"req_solve-dungeon-g-6e33f": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[-1",
+path: "Solve Dungeon Game Dynamic Request - [[-1",
+pathFormatted: "req_solve-dungeon-g-6e33f",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[-1",
+    "numberOfRequests": {
+        "total": "88",
+        "ok": "0",
+        "ko": "88"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "5",
+        "ok": "-",
+        "ko": "5"
+    },
+    "meanResponseTime": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "standardDeviation": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 88,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.727",
+        "ok": "-",
+        "ko": "0.727"
+    }
+}
+    },"req_solve-dungeon-g-f5b33": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[1",
+path: "Solve Dungeon Game Dynamic Request - [[1",
+pathFormatted: "req_solve-dungeon-g-f5b33",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[1",
+    "numberOfRequests": {
+        "total": "174",
+        "ok": "0",
+        "ko": "174"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "meanResponseTime": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "standardDeviation": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 174,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "1.438",
+        "ok": "-",
+        "ko": "1.438"
+    }
+}
+    },"req_solve-dungeon-g-313ed": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[0]]",
+path: "Solve Dungeon Game Dynamic Request - [[0]]",
+pathFormatted: "req_solve-dungeon-g-313ed",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[0]]",
+    "numberOfRequests": {
+        "total": "87",
+        "ok": "0",
+        "ko": "87"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "meanResponseTime": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "standardDeviation": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 87,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.719",
+        "ok": "-",
+        "ko": "0.719"
+    }
+}
+    },"req_solve-dungeon-g-03189": {
+        type: "REQUEST",
+        name: "Solve Dungeon Game Dynamic Request - [[-100",
+path: "Solve Dungeon Game Dynamic Request - [[-100",
+pathFormatted: "req_solve-dungeon-g-03189",
+stats: {
+    "name": "Solve Dungeon Game Dynamic Request - [[-100",
+    "numberOfRequests": {
+        "total": "87",
+        "ok": "0",
+        "ko": "87"
+    },
+    "minResponseTime": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "maxResponseTime": {
+        "total": "4",
+        "ok": "-",
+        "ko": "4"
+    },
+    "meanResponseTime": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "standardDeviation": {
+        "total": "1",
+        "ok": "-",
+        "ko": "1"
+    },
+    "percentiles1": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles2": {
+        "total": "2",
+        "ok": "-",
+        "ko": "2"
+    },
+    "percentiles3": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "percentiles4": {
+        "total": "3",
+        "ok": "-",
+        "ko": "3"
+    },
+    "group1": {
+    "name": "t < 800 ms",
+    "htmlName": "t < 800 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group2": {
+    "name": "800 ms <= t < 1200 ms",
+    "htmlName": "t >= 800 ms <br> t < 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group3": {
+    "name": "t >= 1200 ms",
+    "htmlName": "t >= 1200 ms",
+    "count": 0,
+    "percentage": 0
+},
+    "group4": {
+    "name": "failed",
+    "htmlName": "failed",
+    "count": 87,
+    "percentage": 100
+},
+    "meanNumberOfRequestsPerSecond": {
+        "total": "0.719",
+        "ok": "-",
+        "ko": "0.719"
+    }
+}
+    }
+}
+
+}
+
+function fillStats(stat){
+    $("#numberOfRequests").append(stat.numberOfRequests.total);
+    $("#numberOfRequestsOK").append(stat.numberOfRequests.ok);
+    $("#numberOfRequestsKO").append(stat.numberOfRequests.ko);
+
+    $("#minResponseTime").append(stat.minResponseTime.total);
+    $("#minResponseTimeOK").append(stat.minResponseTime.ok);
+    $("#minResponseTimeKO").append(stat.minResponseTime.ko);
+
+    $("#maxResponseTime").append(stat.maxResponseTime.total);
+    $("#maxResponseTimeOK").append(stat.maxResponseTime.ok);
+    $("#maxResponseTimeKO").append(stat.maxResponseTime.ko);
+
+    $("#meanResponseTime").append(stat.meanResponseTime.total);
+    $("#meanResponseTimeOK").append(stat.meanResponseTime.ok);
+    $("#meanResponseTimeKO").append(stat.meanResponseTime.ko);
+
+    $("#standardDeviation").append(stat.standardDeviation.total);
+    $("#standardDeviationOK").append(stat.standardDeviation.ok);
+    $("#standardDeviationKO").append(stat.standardDeviation.ko);
+
+    $("#percentiles1").append(stat.percentiles1.total);
+    $("#percentiles1OK").append(stat.percentiles1.ok);
+    $("#percentiles1KO").append(stat.percentiles1.ko);
+
+    $("#percentiles2").append(stat.percentiles2.total);
+    $("#percentiles2OK").append(stat.percentiles2.ok);
+    $("#percentiles2KO").append(stat.percentiles2.ko);
+
+    $("#percentiles3").append(stat.percentiles3.total);
+    $("#percentiles3OK").append(stat.percentiles3.ok);
+    $("#percentiles3KO").append(stat.percentiles3.ko);
+
+    $("#percentiles4").append(stat.percentiles4.total);
+    $("#percentiles4OK").append(stat.percentiles4.ok);
+    $("#percentiles4KO").append(stat.percentiles4.ko);
+
+    $("#meanNumberOfRequestsPerSecond").append(stat.meanNumberOfRequestsPerSecond.total);
+    $("#meanNumberOfRequestsPerSecondOK").append(stat.meanNumberOfRequestsPerSecond.ok);
+    $("#meanNumberOfRequestsPerSecondKO").append(stat.meanNumberOfRequestsPerSecond.ko);
+}
